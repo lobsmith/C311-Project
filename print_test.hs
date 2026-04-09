768e37490ee4a77de900e5bf904226cf11c441b3
Lorrin Smith <165953346+lobsmith@users.noreply.github.com>
2026-04-09 15:04:11 -0400

    Enhance whitespace trimming and print translation in print_test.hs
    Refactor whitespace handling functions and improve print statement translation logic.

--- print_test.hs
+++ print_test.hs
@@ -1,8 +1,12 @@
 -- FULL PROGRAM FOR TRANSLATING print("Hello world")
 import System.IO                            -- needed for OpenFile
 import Data.List (isPrefixOf)               -- needed to identify leading keywords such as "print"
-import Data.Char (isSpace)                  -- needed to trim leading white space
+import Data.Char (isSpace)                  -- needed to trim leading and trailing white space
+import Data.List (dropWhileEnd)             -- needed to trim trailing white space
 import Data.Char (isPunctuation, isSymbol)  -- needed to check for special characters
+import Data.List (delete, stripPrefix)      -- delete removes first occurrence of substring
+import Data.List.Split (splitOn)            -- needed for string extraction (concat)
+import qualified Data.Text as T             -- (breakOn, append, drop, length)
 
 -- read one line of Python code (print("Hello World"))
 readLine :: FilePath -> IO String
@@ -13,33 +17,46 @@ readLine py = do
 
 -- simple function that removes leading whitespace from a line of code
 -- key benefit: helps eliminate indentation when looking for prefix keywords
-noSpace :: String -> String
-noSpace = dropWhile isSpace
+noLeadingSpace :: String -> String
+noLeadingSpace = dropWhile isSpace
+
+-- simple function that removes trailing whitespace from a line of code
+noTrailingSpace :: String -> String
+noTrailingSpace = dropWhileEnd isSpace
+
+-- simple function that determines whether a line of code is empty
+isEmpty :: String -> Bool
+isEmpty str = all isSpace str
 
 -- function that removes code that is not a string literal or a variable
 -- Purpose: to display a string literal or variable in MIPS without the surrounding Python syntax
--- CODE IS NOT COMPLETE (JUST A FUNCTION SHELL)
-extractString :: String -> IO String
+extractString :: String -> IO String -- String -> IO String
 extractString str = do
-    return str
+    let extracted = maybe str id (stripPrefix "print(" str)
+    
+    -- ADD CODE TO REMOVE COMMENTS (#)
+    
+    -- remove trailing space and final parenthesis
+    let extracted1 = noTrailingSpace extracted
+    return (init extracted1)
 
 -- function that checks for special characters in variable declarations at a certain index
 isSpecial :: String -> Int -> Bool
 isSpecial str index =
     let char = str !! index in
-        if (char == '\"')    -- print statements have to work if they begin with double quotes
+        if (char == '\"')    -- exclude double quotes, since they are needed for string literals
             then False
         else (isPunctuation char || isSymbol char)
     -- in char /= '\"' && (isPunctuation char || isSymbol char)
 
--- function that translates a print statement into assembly code and writes into MIPS (very rough)
+-- function that translates a print statement into assembly code and writes into MIPS
 translatePrint :: String -> FilePath -> FilePath -> IO()
 translatePrint str mData mCode = do
     -- write .data translation
     appendFile mData ("strName: .asciiz \"" ++ str ++ "\"\n")
     
     -- may use or not (YET TO BE DETERMINED)
-    let printInstr = ("print(" ++ str ++ ")")
+    -- let printInstr = ("print(" ++ str ++ ")")
     
     -- write .text translation
     appendFile mCode ("\tla $a0, strName\n" ++ "\tli $v0, 4\n")
@@ -67,7 +84,14 @@ main = do
     
     -- read first line and trim leading whitespace
     pyLine <- readLine python
-    let trimmedLine = noSpace pyLine
+    let trimmedLine = noLeadingSpace pyLine
+    
+    -- WILL LIKELY MODIFY TO A WHILE LOOP
+    if (isEmpty trimmedLine) then -- OR
+        putStrLn $ "Encountered an empty line"
+    else do putStr $ ""
+    
+    -- IF CHARACTER AT trimmedLine INDEX 0 IS '#', THEN IGNORE COMPLETELY AND MOVE TO THE NEXT LINE
     
     -- NEED TO ACCOUNT FOR INDENTATION IN FUTURE TESTS
     -- MAY MAKE isPrint A GENERIC RESULT VARIABLE (INT)
@@ -77,7 +101,8 @@ main = do
     
     -- trim whitespace and find whether "print" is a prefix of the full string
     let isPrint = "print(" `isPrefixOf` trimmedLine && not (isSpecial trimmedLine 6) -- result is a boolean variable
-
+    -- && (")" `isPostfixOf` trimmedLine && not (isSpecial trimmedLine (length trimmedLine - 2)))
+    
     -- THIS SECTION MAY BECOME A LENGTHY CASE STATEMENT TESTING isPrint/result AT SOME POINT
     -- display whether the line has "print" as a prefix
     if isPrint then do
@@ -88,6 +113,8 @@ main = do
     else    -- A LOT OF CODE WILL BE ADDED LATER
         putStrLn "Not a print statement (code abstracted)"
     
+    -- _ <- hGetLine python -- Moves the file pointer to the next line
+    
     -- write end program logic into code.txt
     appendFile mipsCode "\tjr $ra"
     
@@ -96,3 +123,4 @@ main = do
     codeContents <- readFile mipsCode
     writeFile mipsFinal (dataContents ++ "\n" ++ codeContents)
     putStrLn "Writing to assembly.s successful"
+    
